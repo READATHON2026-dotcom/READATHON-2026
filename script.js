fb3fe17c1ad0a3e33a0de4ef3bfe14951af6dfa2
READATHON2026-dotcom <preetiyadu151015@gmail.com>
2026-07-05 17:33:21 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
 const API_URL = "https://script.google.com/macros/s/AKfycby2yVYxKopIBeqN2BAeaGCw4zlQC6n-wZwnsTyddCKjZyYwKyD-q2UZAh-pFYub_ZdiNQ/exec";
 
-fetch(API_URL)
-  .then(response => response.json())
-  .then(data => {
+document.addEventListener("DOMContentLoaded", () => {
 
-    document.getElementById("totalReaders").textContent = data.totalReaders;
-    document.getElementById("totalBooks").textContent = data.totalBooks;
-    document.getElementById("totalPages").textContent = data.totalPages;
-    document.getElementById("totalMinutes").textContent = data.totalMinutes;
+  fetch(API_URL)
+    .then(response => response.json())
+    .then(data => {
 
-  })
-  .catch(error => {
-    console.error(error);
-  });
+      document.getElementById("totalReaders").textContent = data.totalReaders;
+      document.getElementById("totalBooks").textContent = data.totalBooks;
+      document.getElementById("totalPages").textContent = data.totalPages;
+      document.getElementById("totalMinutes").textContent = data.totalMinutes;
+
+    })
+    .catch(error => console.error(error));
+
+});
