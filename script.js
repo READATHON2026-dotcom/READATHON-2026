b89a01db55d8771f8e9f9dcc32091fb115d25c9b
READATHON2026-dotcom <preetiyadu151015@gmail.com>
2026-07-05 17:12:53 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,13 @@
 const API_URL = "https://script.google.com/macros/s/AKfycby2yVYxKopIBeqN2BAeaGCw4zlQC6n-wZwnsTyddCKjZyYwKyD-q2UZAh-pFYub_ZdiNQ/exec";
 
 fetch(API_URL)
-  .then(response => {
-    if (!response.ok) {
-      throw new Error("Network response was not OK");
-    }
-    return response.json();
-  })
+  .then(response => response.json())
   .then(data => {
-    const totalReaders = document.getElementById("totalReaders");
-    if (totalReaders) {
-      totalReaders.textContent = data.totalReaders;
-    }
+
+    document.getElementById("totalReaders").textContent = data.totalReaders;
+    document.getElementById("totalBooks").textContent = data.totalBooks;
+    document.getElementById("totalPages").textContent = data.totalPages;
+    document.getElementById("totalMinutes").textContent = data.totalMinutes;
+
   })
-  .catch(error => {
-    console.error("Error loading data:", error);
-    const totalReaders = document.getElementById("totalReaders");
-    if (totalReaders) {
-      totalReaders.textContent = "Error";
-    }
-  });
+  .catch(err => console.log(err));
