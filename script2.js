c6f506ee8a3df2af650249e07ac19200bcd870aa
READATHON2026-dotcom <preetiyadu151015@gmail.com>
2026-07-06 12:21:10 +0530

    Update script2.js

--- script2.js
+++ script2.js
@@ -1,1 +1,15 @@
+<script>
+const sections = document.querySelectorAll("section");
 
+function reveal(){
+    sections.forEach(sec=>{
+        const rect = sec.getBoundingClientRect();
+        if(rect.top < window.innerHeight - 100){
+            sec.classList.add("show");
+        }
+    });
+}
+
+window.addEventListener("scroll",reveal);
+window.addEventListener("load",reveal);
+</script>
