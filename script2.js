cb9cc3864772368ac4b95037cbbadba7031a862e
READATHON2026-dotcom <preetiyadu151015@gmail.com>
2026-07-07 00:23:51 +0530

    Update script2.js

--- script2.js
+++ script2.js
@@ -766,3 +766,17 @@ function downloadCertificate() {
   });
 }
 </script>
+window.addEventListener("load", function () {
+  const loader = document.getElementById("loader");
+
+  if (loader) {
+    setTimeout(() => {
+      loader.style.opacity = "0";
+      loader.style.transition = "0.5s ease";
+
+      setTimeout(() => {
+        loader.style.display = "none";
+      }, 500);
+    }, 800);
+  }
+});
